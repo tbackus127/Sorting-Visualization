9f307b5c11a241e7229ebb25614e65df56d14ebc
Tim Backus <tbackus127@gmail.com>
2015-09-02 23:16:32 -0400

    Implemented MergeSort. TODO: Prevent screen from re-getting maxValue and bumping the bars down.

---
+++ src/com/rath/sorts/MergeSort.java
@@ -0,0 +1,65 @@
+/**
+ * Graphical Sorter - MergeSort
+ * @author Tim Backus
+ * @version rev001
+ * <p>
+ * This class serves as an example of how to make sorting algorithms for this program.
+ * </p>
+ */
+
+package com.rath.sorts;
+
+import com.rath.elem.ArrayMemberList;
+
+public class MergeSort {
+  
+  private final ArrayMemberList array;
+  private final int[] helperArray;
+  
+  /**
+   * Default constructor
+   * @param argArray a reference to the ArrayMemberList to be sorted
+   * All sorts must have a constructor Constructor(ArrayMemberList) to be considered valid.
+   */
+	public MergeSort(ArrayMemberList argArray) {
+		array = argArray;
+    helperArray = new int[array.getSize()];
+	}
+	
+  /**
+   * Perform the sort.
+   */
+  public void sort() {
+    mergeSort(0, array.getSize());
+	}
+  
+  private void mergeSort(int startPos, int endPos) {
+    if(endPos - startPos < 2)
+      return;
+    
+    int midPos = (startPos + endPos) / 2;
+    mergeSort(startPos, midPos);
+    mergeSort(midPos, endPos);
+    merge(startPos, midPos, endPos);
+  }
+  
+  private void merge(int left, int mid, int right) {
+    System.err.println("Merge(" + left + ", " + mid + ", " + right + ")");
+    int leftPointer = left;
+    int rightPointer = mid;
+    
+    for(int i = left; i < right; i++) {
+      if(leftPointer < mid && (rightPointer >= right || array.compare(leftPointer, rightPointer) <= 0)) {
+        helperArray[i] = array.getValue(leftPointer);
+        leftPointer++;
+      } else {
+        helperArray[i] = array.getValue(rightPointer);
+        rightPointer++;
+      }
+    }
+    
+    for(int i = left; i < right; i++) {
+      array.setValue(i, helperArray[i]);
+    }
+  }
+}
