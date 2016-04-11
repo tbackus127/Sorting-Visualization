757d713f95a21ffb45dc50b0909b5d67a5e2c0df
Tim Backus <tbackus127@gmail.com>
2016-04-11 19:41:05 -0400

    Cleanup.
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/sorts/RathSort.java
+++ src/com/rath/sorts/RathSort.java
@@ -9,13 +9,24 @@ import com.rath.util.ComponentBuilder;
 
 public class RathSort {
   
+  /** The array wrapper */
   protected ArrayMemberList array;
+  
+  /** Needed for stopping the algorithm, since SwingWorker.cancel() doesn't. */
   protected Thread sortThread;
   
-  // key;type;label;[choices]
+  /** The option map (opt) is built from this 
+   * Syntax: "key;type;label[|choice-N]"
+   */
   protected String[] optsString;
+  
+  /** The option map. Use opt.get(KEY).<val() or sel()> to access */
   protected TreeMap<String, OptionComponent> opt = null;
   
+  /**
+   * Gets the components to add to the options GUI
+   * @return an array of OptionComponents
+   */
   public OptionComponent[] getOptions() {
     OptionComponent[] result = new OptionComponent[opt.size()];
     int i = 0;
@@ -25,21 +36,27 @@ public class RathSort {
     return result;
   }
   
-  public void sort() {
-    sortThread = Thread.currentThread();
-  }
-  
+  /**
+   * Automatically builds the option components from a string array.
+   */
   protected void enableOptions() {
     opt = ComponentBuilder.build(optsString);
   }
   
-  protected void setThread(Thread t) {
-    sortThread = t;
+  /**
+   * Called from child class to enable stopping with the [Stop] button.
+   */
+  public void sort() {
+    sortThread = Thread.currentThread();
   }
   
-  // Yes, I know Thread.stop() is deprecated and unsafe. However, it's the only way it'll actually stop.
-  // We can't have bogosort running until completion every time we want an algorithm to stop.
+  /**
+   * Forcefully stops the sort, because SwingWorker.cancel() doesn't.
+   */
   public void stop() {
+    
+    // Yes, I know Thread.stop() is deprecated and unsafe. However, it's the only way it'll actually stop.
+    // We can't have bogosort running until completion every time we want an algorithm to stop.
     sortThread.stop();
   }
 }
