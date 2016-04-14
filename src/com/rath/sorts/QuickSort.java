843e7d231fa98dbc312c863eb8769ab31bcf4b65
Tim Backus <tbackus127@gmail.com>
2016-04-13 21:42:46 -0400

    Implemented pivot rule in QuickSort (Last is currently broken).
    Signed-off-by: Tim Backus <tbackus127@gmail.com>

--- src/com/rath/sorts/QuickSort.java
+++ src/com/rath/sorts/QuickSort.java
@@ -15,8 +15,11 @@ import com.rath.elem.ArrayMemberList;
 
 public class QuickSort extends RathSort {
   
-  private int pivotRule = 0;
   private final Random rand;
+  private int pointerRule = 0;
+  private int pivotRule = 0;
+  private boolean multithreaded = false;
+  private boolean ternaryMode = false;
   
   /**
    * Default constructor
@@ -25,7 +28,7 @@ public class QuickSort extends RathSort {
    */
 	public QuickSort(ArrayMemberList argArray) {
 		array = argArray;
-    optsString = new String[]{"pnts;sel;Pointer rule: |LR|LL", "pivr;sel;Pivot rule: |First|Last|Middle|Random|Median of Three|Median of Five",
+    optsString = new String[]{"pnts;sel;Pointer rule: |Left+Right|Left x2|Dual Pivot", "pivr;sel;Pivot rule: |First|Last|Middle|Random|Median of Three",
                               "mthd;chk;Multithreaded? ", "tern;chk;Ternary? "};
     enableOptions();
     rand = new Random();
@@ -36,23 +39,76 @@ public class QuickSort extends RathSort {
    */
   public void sort() {
     super.sort();
-    System.err.println("Using pointer rule " + opt.get("pnts").sel());
     pivotRule = opt.get("pivr").sel();
-    quicksort(0, array.getSize() - 1);
+    pointerRule = opt.get("pnts").sel();
+    multithreaded = opt.get("mthd").val();
+    ternaryMode = opt.get("tern").val();
+    
+    switch(pointerRule) {
+      
+      // Left x2
+      case 1:
+        quicksortLL(0, array.getSize() - 1);
+        break;
+      default:
+        quicksortLR(0, array.getSize() - 1);
+    }
 	}
   
-  private void quicksort(int left, int right) {
-    int mid = partition(left, right);
-    if(left < mid - 1)
-      quicksort(left, mid - 1);
-    if(mid < right)
-      quicksort(mid, right);
+  /**
+   * Selects the pivot
+   * @param left the leftmost index
+   * @param right the rightmost index
+   * @return the pivot index as an int
+   */
+  private int getPivot(int left, int right) {
+    switch(pivotRule) {
+      // Last
+      case 1:
+        return right;
+      // Mid
+      case 2:
+        return (right - left) / 2 + left;
+      // Rand
+      case 3:
+        return rand.nextInt(right - left) + left;
+      // Med3
+      case 4:
+        int mid = (left + right) / 2;
+        if(array.compare(right, left) < 0)
+          array.swap(left, right);
+        if(array.compare(mid, left) < 0)
+          array.swap(mid, left);
+        if(array.compare(right, mid) < 0)
+          array.swap(right, mid);
+        return mid;
+      default:
+        return left;
+    }
   }
   
-  private int partition(int left, int right) {
+  /**
+   * Left + Right pointer quicksort
+   * @param left the left bound
+   * @param right the right bound
+   */
+  private void quicksortLR(int left, int right) {
+    int mid = partitionLR(left, right);
+    if(left < mid - 1)
+      quicksortLR(left, mid - 1);
+    if(mid < right)
+      quicksortLR(mid, right);
+  }
+  
+  /**
+   * Left + Right pointer partitioning
+   * @param left the left bound
+   * @param right the right bound
+   */
+  private int partitionLR(int left, int right) {
     int first = left;
     int last = right;
-    int pivot = array.getValue(left);
+    int pivot = array.getValue(getPivot(left, right));
     
     while(first <= last) {
       while (array.compareIndexWithConstant(first, pivot) < 0)
@@ -69,6 +125,31 @@ public class QuickSort extends RathSort {
     }
     
     return first;
+  }
+  
+  private void quicksortLL(int left, int right) {
+    if(left < right) {
+      int mid = partitionLL(left, right);
+      quicksortLL(left, mid - 1);
+      quicksortLL(mid, right);
+    }
+  }
+  
+  private int partitionLL(int left, int right) {
+    int first = left;
+    int pivIdx = getPivot(left, right);
+    int pivot = array.getValue(pivIdx);
+    
+    array.swap(pivot, right);
+    
+    int index = first;
+    for(int i = first; i < right; i++) {
+      if(array.compareIndexWithConstant(i, pivIdx) <= 0)
+        array.swap(first, index++);
+    }
+    
+    array.swap(index, right);
+    return index;
   }
 }
 
